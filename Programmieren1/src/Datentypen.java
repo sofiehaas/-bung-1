cea2b445d1116ad1f6f11f6f01472e3affc838e4
sofiehaas <sofiehaas@141.19.158.133>
2018-10-17 09:48:21 +0200

    Programmieren 1

---
+++ Programmieren1/src/Datentypen.java
@@ -0,0 +1,10 @@
+
+public class Datentypen {
+	public static void main (String [] args) {
+		int a=50;
+		int dollar=0.86;
+		int euro=0.86*50;
+		System.out.println("Dollar in Euro" +euro);
+		}
+	}
+}
